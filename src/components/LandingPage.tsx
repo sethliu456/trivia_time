9ba1b9ed41c9b22eb9f8027cf962cb5f21c3b833
sethliu456 <sethliu456@gmail.com>
2023-05-05 14:35:50 -0700

    add typescript-nocheck

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { useState, useEffect, useRef } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import React from 'react'
--- src/components/QuestionAndAnswer.tsx
+++ src/components/QuestionAndAnswer.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React from "react";
 import {useState, useRef} from 'react'
 import { v4 as uuidv4 } from 'uuid'
