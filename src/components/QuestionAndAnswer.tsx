0d89aba94f77f9ee093cf90a1f01817f3ce9c9b8
sethliu456 <sethliu456@gmail.com>
2023-05-27 20:51:14 -0600

    Fix quotations use dangerouslySetInnerHTML

--- src/components/QuestionAndAnswer.tsx
+++ src/components/QuestionAndAnswer.tsx
@@ -40,7 +40,7 @@ function QuestionAndAnswer({ questionItem, currentQuestion, setCurrentQuestion, 
   return (
     <div className="question-and-answer">
       <h2>Question {currentQuestion + 1}</h2>
-      <h3>{questionItem.question}</h3>
+      <h3 dangerouslySetInnerHTML={{ __html: questionItem.question }} />
       {userAnswered ? <div>{userAnsweredCorrect ? <div style={{fontSize: '2em', color:'#80ed99', fontWeightL: 'bold'}}>Correct!</div>: <div style={{fontSize: '2em', color:'#e84a4a', fontWeightL: 'bold'}}>Incorrect!</div>}</div> : ""}
       {allAnswers.map(answer => <div onClick={userAnswered ? null : handleUserClick} key={uuidv4()} className={`answer ${userAnswered && answer === questionItem.correct_answer ? 'correct' : ''} ${userAnswered && userSelectedAnswer === answer && answer !== questionItem.correct_answer ? 'incorrect' : ''}`} data-value={answer}>{answer}</div>)}
       {userAnswered ? <div onClick={updateCurrentQuestion} className="next-button">Next Question</div> : ""}
